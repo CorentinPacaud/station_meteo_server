7c75f38a027c64b22317a6c9b1657d4efcb912ef
Corentin PACAUD <pacaudcorentin.contact@gmail.com>
2018-10-14 23:58:07 +0200

    Add mix max ext

--- node-js-getting-started/index.js
+++ node-js-getting-started/index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 app.get('/info', function (req, res) {
 
-    var promiseMinMax = new Promise(function (resolve, reject) {
+    var promiseIntMinMax = new Promise(function (resolve, reject) {
         https.get('https://api.thingspeak.com/channels/489319/fields/1.json?api_key=E75SF9QJIJ77J7W6&days=1', (resp) => {
             let data = '';
 
@@ -34,15 +34,49 @@ app.get('/info', function (req, res) {
                 console.log(JSON.parse(data));
                 result = JSON.parse(data);
                 let maxField1 = result.feeds.reduce(function (a, b) {
-                    console.log(a.field1 + "  " + b.field1)
                     return (a.field1 > b.field1) ? a : b;
                 });
                 console.log("MAX: ", maxField1);
                 let minField1 = result.feeds.reduce(function (a, b) {
-                    console.log(a.field1 + "  " + b.field1)
                     return (a.field1 < b.field1) ? a : b;
                 });
-                resolve({ "max_int_temp": maxField1, "min_int_field1": minField1 });
+                resolve({ "max_int_temp": maxField1, "min_int_temp": minField1 });
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            reject(err);
+        });
+    });
+
+    
+    var promiseExtMinMax = new Promise(function (resolve, reject) {
+        https.get('https://api.thingspeak.com/channels/489319/fields/2.json?api_key=E75SF9QJIJ77J7W6&days=1', (resp) => {
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log(JSON.parse(data));
+                result = JSON.parse(data);
+                var maxField2 = result.feeds.reduce(function (a, b) {
+                   // console.log(a.field2 + "  " + b.field2)
+                    return (a.field1 > b.field1) ? a : b;
+                });
+                var minField2 = result.feeds.reduce(function (a, b) {
+                   // console.log(a.field2 + "  " + b.field2)
+                    return (a.field1 < b.field1) ? a : b;
+                });
+                console.log("maxF2: ",maxField2);
+                if(maxField2.field2 == null){
+                    maxField2.field2 = "99";
+                    minField2.field2 = "99";
+                }
+                resolve({ "max_ext_temp": maxField2, "min_ext_temp": minField2 });
             });
 
         }).on("error", (err) => {
@@ -73,7 +107,7 @@ app.get('/info', function (req, res) {
     });
 
 
-    Promise.all([promiseMinMax, promiseExt])
+    Promise.all([promiseIntMinMax,promiseExtMinMax, promiseExt])
         .then(result => {
             console.log("RESULT :", result);
             res.json(result);

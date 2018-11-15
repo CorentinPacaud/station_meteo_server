b712e251a632d221ca69e65a9659ea4f7c95998b
Corentin PACAUD <pacaudcorentin.contact@gmail.com>
2018-11-15 01:54:32 +0100

    toto

--- node-js-getting-started/index.js
+++ node-js-getting-started/index.js
@@ -33,14 +33,18 @@ app.get('/info', function (req, res) {
             resp.on('end', () => {
                 console.log(JSON.parse(data));
                 result = JSON.parse(data);
-                let maxField1 = result.feeds.reduce(function (a, b) {
-                    return (a.field1 > b.field1) ? a : b;
-                });
-                console.log("MAX: ", maxField1);
-                let minField1 = result.feeds.reduce(function (a, b) {
-                    return (a.field1 < b.field1) ? a : b;
-                });
-                resolve({ "max_int_temp": maxField1, "min_int_temp": minField1 });
+                if (result.feeds.length > 0) {
+                    let maxField1 = result.feeds.reduce(function (a, b) {
+                        return (a.field1 > b.field1) ? a : b;
+                    });
+                    console.log("MAX: ", maxField1);
+                    let minField1 = result.feeds.reduce(function (a, b) {
+                        return (a.field1 < b.field1) ? a : b;
+                    });
+                    resolve({ "max_int_temp": maxField1, "min_int_temp": minField1 });
+                } else {
+                    resolve({ "max_int_temp": "99", "min_int_temp": "99" });
+                }
             });
 
         }).on("error", (err) => {
@@ -49,7 +53,7 @@ app.get('/info', function (req, res) {
         });
     });
 
-    
+
     var promiseExtMinMax = new Promise(function (resolve, reject) {
         https.get('https://api.thingspeak.com/channels/489319/fields/2.json?api_key=E75SF9QJIJ77J7W6&days=1', (resp) => {
             let data = '';
@@ -63,20 +67,24 @@ app.get('/info', function (req, res) {
             resp.on('end', () => {
                 console.log(JSON.parse(data));
                 result = JSON.parse(data);
-                var maxField2 = result.feeds.reduce(function (a, b) {
-                   // console.log(a.field2 + "  " + b.field2)
-                    return (a.field1 > b.field1) ? a : b;
-                });
-                var minField2 = result.feeds.reduce(function (a, b) {
-                   // console.log(a.field2 + "  " + b.field2)
-                    return (a.field1 < b.field1) ? a : b;
-                });
-                console.log("maxF2: ",maxField2);
-                if(maxField2.field2 == null){
-                    maxField2.field2 = "99";
-                    minField2.field2 = "99";
+                if (result.feeds.length > 0) {
+                    var maxField2 = result.feeds.reduce(function (a, b) {
+                        // console.log(a.field2 + "  " + b.field2)
+                        return (a.field1 > b.field1) ? a : b;
+                    });
+                    var minField2 = result.feeds.reduce(function (a, b) {
+                        // console.log(a.field2 + "  " + b.field2)
+                        return (a.field1 < b.field1) ? a : b;
+                    });
+                    console.log("maxF2: ", maxField2);
+                    if (maxField2.field2 == null) {
+                        maxField2.field2 = "99";
+                        minField2.field2 = "99";
+                    }
+                    resolve({ "max_ext_temp": maxField2, "min_ext_temp": minField2 });
+                } else {
+                    resolve({ "max_ext_temp": "99", "min_ext_temp": "99" });
                 }
-                resolve({ "max_ext_temp": maxField2, "min_ext_temp": minField2 });
             });
 
         }).on("error", (err) => {
@@ -106,8 +114,28 @@ app.get('/info', function (req, res) {
         });
     });
 
+    var promiseWeather = new Promise(function (resolve, reject) {
+        https.get('http://api.openweathermap.org/data/2.5/weather?q=Lyon&units=metric&APPID=59dec7514a208a87bdb1a6e130d48cb7', (resp) => {
+            let data = '';
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
 
-    Promise.all([promiseIntMinMax,promiseExtMinMax, promiseExt])
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log(JSON.parse(data));
+
+                resolve({ "weather": { "weather": data.weather.id, "sunset": formatSunRiseSet(data.sys.sunset), "sunrise": formatSunRiseSet(data.sys.sunrise) } });
+            });
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            reject(err);
+        });
+    });
+
+
+    Promise.all([promiseIntMinMax, promiseExtMinMax, promiseExt, promiseWeather])
         .then(result => {
             console.log("RESULT :", result);
             res.json(result);
@@ -117,6 +145,17 @@ app.get('/info', function (req, res) {
         });
 
 });
+
+
+function formatSunRiseSet(timestamp) {
+    var date = new Date(unix_timestamp * 1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Will display time in 10:30:23 format
+    return hours + ':' + minutes.substr(-2);
+}
 
 app.listen(PORT, function () {
     console.log('Example app listening on port ' + PORT + '!')
